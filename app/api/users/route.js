ba62292066f4eb83302eace1dbff11e14069ddd4
musarri <msaricicek_99@hotmail.com>
2024-07-09 21:44:49 +0300

    model error fixed

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,9 +1,7 @@
-import Category from "../../../model/Category";
 import connectDB from "../../lib/connectDb";
 
 export async function GET(request, route) {
   await connectDB();
-  const users = await User.find({});
 
   return Response.json({ users });
 }
