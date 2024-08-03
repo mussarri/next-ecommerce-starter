ed268fe4db8e22cffa98e00369d5d0b91b484171
musarri <msaricicek_99@hotmail.com>
2024-08-03 11:04:07 +0300

    home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,8 @@
-import ProductCard from "./components/productCard";
 import HomeBanner from "./components/homeBanner";
 import HomeServices from "./components/homeServices";
 import MobileProducts from "./components/mobileProducts";
-import Header from "./components/header";
 
-async function getData() {
-  const res = await fetch(process.env.API_URL + "api/products", {
-    cache: "no-store",
-  });
-  return res.json();
-}
 export default async function Home() {
-  const data = await getData();
   return (
     <>
       <HomeBanner />
