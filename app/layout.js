afb780e6e753ada0766592f5802b6a3b464690ce
musarri <msaricicek_99@hotmail.com>
2024-08-03 14:15:09 +0300

    font removed

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,9 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { GlobalProvider } from "./GlobalProvider";
 import Header from "./components/header";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
@@ -19,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <GlobalProvider>
           <Header />
           {children}
